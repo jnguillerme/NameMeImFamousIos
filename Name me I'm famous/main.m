85f0deedcda5e5f3fbf832b4f2239407416fa46e
Jino <jnguillerme@Jinos-Mac.local>
2012-11-12 15:21:54 +0000

    Initial Commit

---
+++ Name me I'm famous/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Name me I'm famous
+//
+//  Created by Jino on 12/11/2012.
+//  Copyright (c) 2012 nmif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "nmifAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([nmifAppDelegate class]));
+    }
+}
---
+++ Name me I'm famous/nmifAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  nmifAppDelegate.h
+//  Name me I'm famous
+//
+//  Created by Jino on 12/11/2012.
+//  Copyright (c) 2012 nmif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface nmifAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
